6ae3c2510982e341c8be9286d50fd3cfbabe4b82
Matas <matas.lankelis@gmail.com>
2026-04-24 10:17:42 +0300

    Initial wordle game

---
+++ PVPBack.Core/Realtime/MiniGames/Games/Wordle/WordleGame.cs
@@ -0,0 +1,35 @@
+﻿namespace PVPBack.Core.Realtime.MiniGames.Games.Wordle;
+
+public class WordleGame : IMiniGame
+{
+    private List<PlayerRuntime> _players = new()
+    public bool IsCompleted { get; private set; }
+    public bool IsFailed { get; private set; }
+            
+    private const int MaxMistakes = 3;
+    private int _mistakeCount = 0;
+    public void Start(List<PlayerRuntime> players)
+    {
+        throw new NotImplementedException();
+    }
+
+    public GameActionResult SubmitAction(PlayerRuntime player, GameAction action)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void RefreshPlayerPrivateData(List<PlayerRuntime> players)
+    {
+        throw new NotImplementedException();
+    }
+
+    public object GetPublicState()
+    {
+        throw new NotImplementedException();
+    }
+
+    
+    
+    
+    
+}
