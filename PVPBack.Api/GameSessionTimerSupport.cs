75e189d637e51342bb58f79b5249cd1ee1172e3b
Matas <matas.lankelis@gmail.com>
2026-05-22 10:02:29 +0300

    Updated given time to 15 minutes

--- PVPBack.Api/GameSessionTimerSupport.cs
+++ PVPBack.Api/GameSessionTimerSupport.cs
@@ -12,7 +12,7 @@ namespace PVPBack.Api;
 public class GameSessionTimerSupport
 {
     private readonly ConcurrentDictionary<string, SessionTimerState> _timers = new();
-    private readonly TimeSpan _duration = TimeSpan.FromMinutes(5);
+    private readonly TimeSpan _duration = TimeSpan.FromMinutes(15);
 
     public bool StartSession(GameSessionRuntime session)
     {
